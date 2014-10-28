a469f7ebddce3dfc142e1bfd85fb248e86c81804
Graham Campbell <graham@mineuk.com>
2014-10-28 12:35:36 +0000

    Fixed another typo

--- tests/Functional/CommandTest.php
+++ tests/Functional/CommandTest.php
@@ -66,6 +66,6 @@ class CommandTest extends AbstractTestCase
 
     protected function getKernel()
     {
-        return $this->app->make('Illuminate\Contracts\Console\Commands\Kernel');
+        return $this->app->make('Illuminate\Contracts\Console\Kernel');
     }
 }
